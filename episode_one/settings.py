b7b6044bf23ec28a32ecd910c8f98c0b2496ec73
Tony Hall <tonyhall@tonys-mbp.lan>
2023-09-21 22:43:52 -0400

    updated v2 app

--- episode_one/settings.py
+++ episode_one/settings.py
@@ -26,8 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
-    SECRET_KEY = f.read().strip()
+# with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+#     SECRET_KEY = f.read().strip()
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+
+
 
 
 
