4d6b3bebbb538cd5114e6bef3f78bef9a042c8d7
Tony Hall <tonyhall@tonys-mbp.lan>
2023-09-23 01:26:03 -0400

    updated v2 app

--- episode_one/settings.py
+++ episode_one/settings.py
@@ -175,6 +175,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # for email confirmations and setup
 # from .secrets import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+###
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -183,6 +184,7 @@ EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 # for api key 
 import dotenv
