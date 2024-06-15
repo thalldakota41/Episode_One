0d7532274f455b9fdfc6a70c1660e9dadf639158
Tony Hall <tonyhall@tonys-mbp.lan>
2024-06-14 23:11:56 -0400

    fixed typo is db config

--- episode_one/settings.py
+++ episode_one/settings.py
@@ -115,7 +115,7 @@ POSTGRES_READY = (
 
 if POSTGRES_READY:
     DATABASES = {
-        'defualt': {
+        'default': {
             'ENGINE': 'django.db.backends.postgresql',
             "NAME": POSTGRES_DB,
             "USER": POSTGRES_USER,
