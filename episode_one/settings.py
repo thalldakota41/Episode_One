44f21ec7ed0ab28a7ad344d01ca32e8f38a1621a
Tony Hall <tonyhall@tonys-mbp.lan>
2024-07-04 22:03:35 -0400

    added to .env file

--- episode_one/settings.py
+++ episode_one/settings.py
@@ -124,52 +124,12 @@ if POSTGRES_READY:
             "PASSWORD": POSTGRES_PASSWORD,
             "HOST": POSTGRES_HOST,
             "PORT": POSTGRES_PORT,
-            # 'OPTIONS': {
-            #     'sslmode': 'require',  # Ensure SSL is required for the connection
-            # },
+            'OPTIONS': {
+                'sslmode': 'require',  # Ensure SSL is required for the connection
+            },
         }
     }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-# DATABASE_URL = os.environ.get('DATABASE_URL')
-
-# if 'DATABASE_URL' in os.environ:
-#     import dj_database_url
-#     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-# else:
-#     POSTGRES_DB = os.environ.get('POSTGRES_DB')
-#     POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-#     POSTGRES_USER = os.environ.get('POSTGRES_USER')
-#     POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
-#     POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
-
-#     POSTGRES_READY = (
-#         POSTGRES_DB is not None
-#         and POSTGRES_PASSWORD is not None
-#         and POSTGRES_USER is not None
-#         and POSTGRES_HOST is not None
-#         and POSTGRES_PORT is not None
-#     )
-
-#     if POSTGRES_READY:
-#         DATABASES = {
-#             'default': {
-#                 'ENGINE': 'django.db.backends.postgresql',
-#                 'NAME': POSTGRES_DB,
-#                 'USER': POSTGRES_USER,
-#                 'PASSWORD': POSTGRES_PASSWORD,
-#                 'HOST': POSTGRES_HOST,
-#                 'PORT': POSTGRES_PORT,
-#                 'OPTIONS': {
-#                     'sslmode': 'require',
-#                 },
-#             }
-#         }
 
 
 # Password validation
